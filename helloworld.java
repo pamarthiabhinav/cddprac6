260e73aee9587223c163f7bb93bd1013a2e7be1e
Abhinav Pamarthi <54942985+pamarthiabhinav@users.noreply.github.com>
2021-10-16 16:29:08 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String args[]) {
+        System.out.println("Hello World, This Is Abhinav");
+        System.out.println("\t\tThis Is For My CDD Practical-6");
+    }
+}
